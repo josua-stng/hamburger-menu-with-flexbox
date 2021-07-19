6fee9c39fe9ba50e3ce5f65a7f3815723904f19a
josua-stng <josuasitanggang51@gmail.com>
2021-07-19 11:41:58 +0700

    add file

---
+++ script.js
@@ -0,0 +1,5 @@
+const menutoogle=document.querySelector(".menu-toogle input")
+const nav=document.querySelector("nav ul");
+menutoogle.addEventListener("click",function(){
+    nav.classList.toggle("slide");
+});
